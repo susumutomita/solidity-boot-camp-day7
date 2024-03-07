bdfb80c87d6650db10d4dcd90ae0ada6ce65fb9e
susumu tomita <oyster880@gmail.com>
2024-03-08 00:02:25 +0900

    update homework

--- src/Query.sol
+++ src/Query.sol
@@ -6,15 +6,11 @@ contract Query {
 
     function checkCall(bytes calldata data) external {
         // ERC20トランスファー関数のセレクタは "a9059cbb"
-        bytes4 selector;
-        assembly {
-            // calldataから最初の4バイト（関数セレクタ）を読み取る
-            selector := calldataload(data.offset)
-        }
+        bytes4 selector = bytes4(data[:4]);
         require(selector == 0xa9059cbb, "Not an ERC20 transfer function");
 
         // `data`からアドレスと金額を抽出
-        (, address to, uint256 amount) = abi.decode(data[4:], (address, uint256));
+        (address to, uint256 amount) = abi.decode(data[4:], (address, uint256));
 
         emit TransferOccurred(to, amount);
     }
